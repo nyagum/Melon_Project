041d22d0a1c8bd2475899bbcc2f356a93b3d29ff
Anya Choi <choi.bosel@gmail.com>
2017-11-08 18:28:37 +0900

    3nd commit add ChartListActivity

---
+++ app/src/main/java/melon/project/com/melon_proj/activity/ChartListActivity.java
@@ -0,0 +1,15 @@
+package melon.project.com.melon_proj.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import melon.project.com.melon_proj.R;
+
+public class ChartListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chart_list);
+    }
+}
