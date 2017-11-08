00a3415acdf1e7db8a76d378df153bf76cf7ef66
JUWON-KEVIN-LEE <quf9345@gmail.com>
2017-11-08 17:36:24 +0900

    today update

--- app/src/main/java/melon/project/com/melon_proj/activity/SongDetailActivity.java
+++ app/src/main/java/melon/project/com/melon_proj/activity/SongDetailActivity.java
@@ -1,7 +1,9 @@
-package melon.project.com.melon_proj;
+package melon.project.com.melon_proj.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import melon.project.com.melon_proj.R;
 
 public class SongDetailActivity extends AppCompatActivity {
 
